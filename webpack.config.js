af77e1b1d76a1387558c218e656a142be6b5169d
KCrandall6 <kcrandall67@gmail.com>
2021-11-30 13:30:12 -0700

    beginning

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    // entry point of our app
+    './client/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
